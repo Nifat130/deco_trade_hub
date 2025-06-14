2eff3721d8f99341c8cd734488b321a3144152ca
msiprime <msisakib958@gmail.com>
2025-06-14 23:57:49 +0600

    banner shimmer is done

--- lib/features/home/presentation/retailer/view/retailer_promotional_banners.dart
+++ lib/features/home/presentation/retailer/view/retailer_promotional_banners.dart
@@ -1,19 +1,23 @@
 import 'package:app_ui/app_ui.dart';
 import 'package:deco_trade_hub/core/shimmers/dummy_db/dummy_product.dart';
+import 'package:deco_trade_hub/core/utils/constants/app_sizer.dart';
 import 'package:deco_trade_hub/features/store/model/store_banner_model.dart';
 import 'package:deco_trade_hub/features/store/presentation/controllers/store_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+import '../../../../../core/utils/constants/app_colors.dart';
+
+final List<String> bannerUrls = [
+  dummyImageUrl,
+  dummyImageUrl,
+  dummyImageUrl,
+  dummyImageUrl,
+  'dummyImageUrl',
+];
 
 class PromotionalBanners extends StatelessWidget {
-  final List<String> bannerUrls = [
-    dummyImageUrl,
-    dummyImageUrl,
-    dummyImageUrl,
-    dummyImageUrl,
-    'dummyImageUrl',
-  ];
-
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -52,7 +56,6 @@ class _TrendingStoresSectionState extends State<TrendingStoresSection> {
   @override
   void initState() {
     _carouselController = CarouselController(initialItem: 0);
-    // add post frame callback to ensure the controller is initialized
     WidgetsBinding.instance.addPostFrameCallback((_) {
       Get.find<StoreController>().fetchTrendingStores();
     });
@@ -63,7 +66,7 @@ class _TrendingStoresSectionState extends State<TrendingStoresSection> {
   Widget build(BuildContext context) {
     return GetBuilder<StoreController>(builder: (storeController) {
       if (storeController.isLoadingTrendingStores || storeController.trendingStoreBanners == null) {
-        return const Center(child: CircularProgressIndicator());
+        return BannerShimmer();
       }
       if (storeController.trendingStoreBanners!.isEmpty) {
         return const SizedBox.shrink();
@@ -73,7 +76,7 @@ class _TrendingStoresSectionState extends State<TrendingStoresSection> {
         child: CarouselView.weighted(
           controller: _carouselController,
           itemSnapping: true,
-          flexWeights: const <int>[ 7, 1],
+          flexWeights: const <int>[7, 1],
           children: storeController.trendingStoreBanners!.map((image) {
             return HeroLayoutCard(storeBanners: image);
           }).toList(),
@@ -129,6 +132,59 @@ class HeroLayoutCard extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class BannerShimmer extends StatelessWidget {
+  const BannerShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.sizeOf(context).width;
+    return Skeletonizer(
+      child: ConstrainedBox(
+          constraints: BoxConstraints(maxHeight: 400 / 1.75),
+          child: Container(
+            width: width.w,
+            margin: EdgeInsets.symmetric(horizontal: 20.w),
+            padding: EdgeInsets.all(8.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Stack(
+                  children: [
+                    ImageAttachmentThumbnail(
+                      height: 120.h,
+                      width: width.w,
+                      fit: BoxFit.cover,
+                      borderRadius: BorderRadius.circular(8),
+                      imageUrl: dummyImageUrl,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8.h),
+                Text(
+                  'product name',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    color: AppColors.primary,
+                  ),
+                ),
+                SizedBox(height: 4.h),
+                Text(
+                  'description of the product goes here',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 12.sp),
+                ),
+                SizedBox(height: 5.h),
+                // SizedBox(height: 6.h),
+              ],
+            ),
+          )),
     );
   }
 }
